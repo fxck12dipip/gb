7d8791c40b07e31b93960ea1a721cd8ee141e5a4
bigskunk <sema.kukureku@gmail.com>
2021-12-27 19:02:14 +0700

    initial commit

---
+++ 1,2.py
@@ -0,0 +1,20 @@
+res = []
+d = dict()
+with open('nginx_logs.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        ln = line.split()
+        ip = ln[0]
+        res.append((ip, ln[5].strip('"'), ln[6]))
+        if ip not in d:
+            d[ip] = 1
+        else:
+            d[ip] += 1
+print(res)
+
+m = 0
+k = ''
+for key, value in d.items():
+    if value > m:
+        k = key
+        m = value
+print(k, m)
